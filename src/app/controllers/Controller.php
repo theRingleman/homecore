360cf7392b11a5bd84579bbc7d9f212a7f139154
Sam Ringleman <sam.rinleman@gmail.com>
2018-03-11 13:35:13 -0600

    add database in contructor function and add the render json function so we can output json when needed

--- src/app/controllers/Controller.php
+++ src/app/controllers/Controller.php
@@ -1,13 +1,33 @@
 <?php
 
 class Controller {
+
+	public $db;
+
+	public function __construct()
+	{
+		$dbCreds = json_decode(file_get_contents(dirname(__DIR__) . '/creds.json'));
+		
+		$this->db = new DB\SQL(
+    		'mysql:host=homestorage;port=3306;dbname=homecore',
+    		"{$dbCreds->database->username}",
+    		"{$dbCreds->database->password}"
+		);
+	}
+
 	public function beforeroute($f3) 
 	{
-		
+
 	}
 
 	public function afterroute($f3) 
 	{
 		
+	}
+
+	public function renderJson($f3, $response)
+	{
+		$f3->set('response', $response);
+		echo \Template::instance()->render('json.php');
 	}
 }
