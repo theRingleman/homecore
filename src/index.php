d6281aac1f8f0d4c2bc6566ccdc5ebc47ecc063f
Sam Ringleman <sam.rinleman@gmail.com>
2018-03-13 17:41:46 -0600

    add gump for validation php-auth for simple auth and add some comments for later

--- src/index.php
+++ src/index.php
@@ -4,12 +4,12 @@ require 'vendor/autoload.php';
 
 $f3 = \Base::instance();
 
+// Add our config
 $f3->config('app/config.ini');
-
+// Add routes
 $f3->config('app/routes.ini');
-
+// Set up our database creds and set DB as an f3 global.
 $dbCreds = json_decode(file_get_contents('creds.json'));
-
 $f3->set('DB', new DB\SQL(
     'mysql:host=homestorage;port=3306;dbname=homecore',
     "{$dbCreds->database->username}",
