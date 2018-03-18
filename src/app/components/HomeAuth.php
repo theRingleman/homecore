b18a4e538cbff14d260813cc24760c97cc3c0680
Sam Ringleman <sam.ringleman@gmail.com>
2018-03-18 10:39:51 -0600

    add docbloc comments

--- src/app/components/HomeAuth.php
+++ src/app/components/HomeAuth.php
@@ -3,18 +3,25 @@
 use Lcobucci\JWT\Builder;
 use Lcobucci\JWT\Signer\Hmac\Sha256;
 use Lcobucci\JWT\Parser;
+use Lcobucci\JWT\ValidationData;
 
 class HomeAuth
 {
     private $token;
     protected $cache;
+    private $validator;
 
     public function __construct()
     {
         $this->cache = new HomeCache;
         $this->signer = new Sha256();
+        $this->validator = new ValidationData();
     }
 
+    /**
+     * Creates the JWT and sets all the claims and signs it.
+     * @throws Exception
+     */
     public function createToken()
     {
         $token =  (new Builder)
@@ -27,6 +34,10 @@ class HomeAuth
         $this->token = $token;
     }
 
+    /**
+     * Stores the token in the cache. We create two values in cache, one keyed by the randomly
+     * generated token id, the other keyed by the tokens user id. This will be used for future validation.
+     */
     private function storeToken()
     {
         $this->cache->setHash("token-{$this->token->getClaim('jti')}", [
@@ -37,24 +48,45 @@ class HomeAuth
         $this->cache->set("token-{$this->user->id}", true);
     }
 
-    public function getToken($user){
+    /**
+     * This returns the newly created token to the requester.
+     * @param $user
+     * @return string
+     * @throws Exception
+     */
+    public function getToken(User $user){
         $this->user = $user;
         $this->createToken();
         $this->storeToken();
         return (string)$this->token;
     }
 
-    public function validate($token)
+    /**
+     * This does the initial token validation, it checks the tokens claims, if it passes validation, then we run
+     * the verify method to verify the signer.
+     * @param $token
+     * @return bool
+     */
+    public function validateToken($token)
     {
-        $token = (new Parser)->parse($token);
-        if ($token->verify($this->signer, "testers")) {
-           $this->token = $token;
-           return true;
-        } else {
-            return false;
-        }
+        $this->validator->setIssuer("Sam Ringleman");
+        $this->token = (new Parser)->parse($token);
+        return $this->token->validate($this->validator) ? $this->verifyToken() : $this->token->validate($this->validator);
     }
 
+    /**
+     * This verifies the tokens signer.
+     * @return mixed
+     */
+    private function verifyToken()
+    {
+        return $this->token->verify($this->signer, 'testers');
+    }
+
+    /**
+     * This allows us to grab the token information from the cache where we store user info.
+     * @return array
+     */
     public function getTokenFromCache()
     {
         return $this->cache->getHash("token-{$this->token->getClaim('jti')}");
